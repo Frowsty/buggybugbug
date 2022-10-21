46ff1d6df15d0de08ee18684baf46fb88bfafde0
Frowsty <daniel_barbis@hotmail.com>
2022-10-21 09:39:31 +0200

    Initial commit of buggy code

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+
+int main (int argc, char **argv)
+{
+	int test = 2;
+
+	if (test == 1)
+		printf("This is one amazing print-statement");
+
+	return 0;	
+}
